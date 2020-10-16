554e8330fda7c1073d2057350cdbb66197a9a47e
buvta <12312540+buvta@users.noreply.github.com>
2020-10-16 22:31:31 +0800

    列自适应方案解决
    去表表格中的width

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import 'modern-normalize'
 import Register from './components/register'
 import './lib/element/index'
 import ElementUI from 'element-ui'
+import Plugin from 'v-fit-columns'
 
+Vue.use(ElementUI)
+Vue.use(Plugin)
 Vue.use(ElementUI)
 Register.registerComponents()
 Vue.config.productionTip = false
