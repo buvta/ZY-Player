6c1962ec81af34d5537003d2d2153a23b4172682
buvta <12312540+buvta@users.noreply.github.com>
2020-10-16 23:01:27 +0800

    引入列自适应方案

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import store from './store'
 import 'modern-normalize'
 import Register from './components/register'
 import './lib/element/index'
+// import { Table, TableColumn, Button, Select, Autocomplete, Option } from 'element-ui' 太麻烦了
 import ElementUI from 'element-ui'
+import Plugin from 'v-fit-columns'
 
 Vue.use(ElementUI)
+Vue.use(Plugin)
 Register.registerComponents()
 Vue.config.productionTip = false
 new Vue({
